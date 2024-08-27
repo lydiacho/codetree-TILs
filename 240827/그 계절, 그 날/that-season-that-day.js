217bf5d9d48dfd1066cf0c3ad2ed555c12a03485
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-08-27 10:23:27 +0900

    Title: 그 계절, 그 날, Time: 223ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240827/그 계절, 그 날/that-season-that-day.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const [y, m, d] = fs.readFileSync(0).toString().trim().split(" ").map(v=>+v);
+
+function isYunNyeon (year) {
+    if( year % 4 === 0) {
+        if (year%100 === 0) {
+            if (year%400===0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+function solution(y, m, d) {
+    const arr = Array.from({length: 13}, (_, i)=> i%2===0 ? 30 : 31);
+    arr[2] = isYunNyeon(y) ? 29 : 28;
+
+    if (d > arr[m]) return -1;
+
+    if (m >= 3 && m <= 5) { 
+        return 'Spring';
+    }
+    if (m >= 6 && m <= 8) {
+        return 'Summer';
+    }
+    if (m >= 9 && m <= 11) {
+        return 'Fall';
+    }
+    return 'Winter';
+}
+
+console.log(solution(y, m, d));
