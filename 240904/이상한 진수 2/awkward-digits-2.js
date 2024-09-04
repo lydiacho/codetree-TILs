b88dbe0dd2a3d6f95a1af492f6a7b303be8e7bc4
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-09-04 14:49:55 +0900

    Title: 이상한 진수 2, Time: 221ms, Memory: 47MB, Status: Passed - CodeTree

--- 240904/이상한 진수 2/awkward-digits-2.js
+++ 240904/이상한 진수 2/awkward-digits-2.js
@@ -6,13 +6,19 @@ const arr = fs.readFileSync(0).toString().trim().split('').map(v=>+v);
 
 function solution(arr) {
     const n = arr.length;
+
     // 첫번째 0 찾아서 1 바꾸기 
     for(let i = 0; i < n; i++) {
         if(arr[i]===0) {
             arr[i] = 1;
             break;
         }
+        // 1밖에 없는 경우 -> 마지막 1을 0으로 
+        if (i===n-1 && arr[i]===1) {
+            arr[i] = 0;
+        }
     }
+    
 
     // 최댓값의 이진법 -> 십진법으로 바꾸기 
     return arr.reduce((acc,curr,idx)=>{
