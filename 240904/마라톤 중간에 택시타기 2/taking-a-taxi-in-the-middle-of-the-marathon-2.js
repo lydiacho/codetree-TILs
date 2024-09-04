3720ac48fd2b723c1163e4e83d8dc42b2753b73b
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-09-04 15:08:14 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240904/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.js
@@ -0,0 +1,25 @@
+// 1번, N번 빼고 체크포인트 하나 건너뛸 때 최소 거리 
+// 점과 점 사이의 거리 |x1 - x2| + |y1 - y2|
+
+const fs = require('fs');
+const [n, ...arr] = fs.readFileSync(0).toString().trim().split('\n');
+const pos = arr.map(v=>v.split(' ').map(w=>+w));
+
+function solution(n, pos) {
+    let min = 4000;
+
+    // 각 체크포인트를 빠질 때 거리 구하기 
+    for (let i = 1; i<n-1; i++) {
+        let dist = 0;
+        for (let j = 0; j < n-1; j++) {
+            if (j===i) continue;
+            const [x1, y1] = pos[j];
+            const [x2, y2] = (j+1===i) ? pos[j+2] : pos[j+1];
+            dist += Math.abs(x1 - x2) + Math.abs(y1 - y2);
+        }
+        min = Math.min(dist, min);
+    }
+    return min;
+}
+
+console.log(solution(n, pos));
