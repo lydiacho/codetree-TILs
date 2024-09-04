139b17f0a760eccd5ef352882517afaf5fd40300
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-09-04 11:04:51 +0900

    Title: 모이자, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240904/모이자/gather.js
+++ 240904/모이자/gather.js
@@ -1,12 +1,13 @@
 // 모든 사람의 이동 거리의 합이 최소 
-const n = 5;
-const A = [1, 2, 3, 2, 6];
+const fs = require('fs');
+const [n, arr] = fs.readFileSync(0).toString().trim().split('\n');
+const A = arr.split(' ').map(v=>+v);
 
 function solution (n, A) {
     let min = 10000;
-    for (let i =  0; i < A.length; i++) {
+    for (let i =  0; i < n; i++) {
         let sum = 0;
-        for (let j = 0; j < A.length; j++) {
+        for (let j = 0; j < n; j++) {
             if (i===j) continue;
             sum += Math.abs(j-i) * A[j];
         }
