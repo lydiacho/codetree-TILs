17f4097eb0028670ccda7aaf8225a573160156ab
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-09-06 21:29:02 +0900

    Title: 낮은 지점들, Time: 455ms, Memory: 52MB, Status: Passed - CodeTree

--- 240906/낮은 지점들/lowest-points.js
+++ 240906/낮은 지점들/lowest-points.js
@@ -5,15 +5,14 @@ const dots = arr.map(v=>v.split(' ').map(w=>+w));
 // 같은 x좌표를 갖는 점들 중 y가 가장 작은 애만 남기기 
 
 function solution(dots) {
-    const table = {};
+    const m = new Map();
     dots.map(v=>{
         const [x,y] = v;
-        if (!table[x]) table[x] = [];
-        table[x].push(y);
+        if (m.has(x)) m.set(x,Math.min(m.get(x),y));
+        else m.set(x,y);
     })
 
-    const mins = Object.values(table).map(v=>Math.min(...v));
-    return mins.reduce((acc,curr)=>acc+curr,0);
+    return Array.from(m.values()).reduce((acc,curr)=>acc+curr,0);
 }
 
 console.log(solution(dots));
